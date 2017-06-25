ef372855a66f9d6daba0c23a3aa2de60df5d032f
Sebastian Salazar <sebastian@lacaja.click>
2017-06-24 20:31:18 -0500

    Character view more button

--- js/characters.js
+++ js/characters.js
@@ -4,7 +4,9 @@ $(document).ready(function() {
 	var marvelAPIURI = "https://gateway.marvel.com/v1/public";
 	var charactersPath = "/characters";
 	var charactersURI = marvelAPIURI + charactersPath;
+	var characterComicsURI = ""; 
 
+	var characterId = 0;
 	var characterName = "";
 	var characterDescription = "";
 	var characterThumbnailPath = "";
@@ -12,6 +14,8 @@ $(document).ready(function() {
 	var characterFirstComicsList = [];
 	var searchTerm = "";
 	var orderVal = "-modified";
+	var comicTitle = "";
+	var comicsList = [];
 	
 	var $charactersContainer = $('.characters-container');
 	var $characterTemplate = $('<li class="col character-container">' +
@@ -20,10 +24,9 @@ $(document).ready(function() {
 											'<div class="col">' +
 												'<img class="character-thumbnail" src="" alt="">' +
 											'</div>' +
-											'<div class="col">' +
+											'<div class="col character-details">' +
 												'<h3 class="character-name"></h3>' +
 												'<p class="character-description"></p>' +
-												'<button>View more</button>' +
 											'</div>' +
 										'</div>' +
 										'<div class="related-comics">' +
@@ -37,9 +40,18 @@ $(document).ready(function() {
 	var $searchButton = $('#search-button');
 	var $searchInput = $('#search-input');
 	var $orderSelect = $('#sort-by');
-
+	var $viewMoreButton = $('<button>View more</button>');
+	var $relatedComics;
+	var $character;
 
 	//FUNCIONES
+	//Agregar botones de ver mas
+	function addViewMoreButtons() {
+		$('.character-details').each( function() {
+			$viewMoreButton.clone(true).appendTo($(this));  //clone(true) clona también los metodos asociados como .click por ejemplo
+		});
+	}
+
 	//Busqueda por defecto
 	function searchDefault() {
 		$.getJSON( charactersURI, {
@@ -50,12 +62,22 @@ $(document).ready(function() {
 			//Para cada resultado
 			$.each(response.data.results, function(index, item) {
 				showFoundCharacter(index, item);
-			});	
+			});
+
+			//Agregar botones de ver mas
+			addViewMoreButtons();
 		});
 	}
 
 	//Mostrar personajes encontrados
 	function showFoundCharacter(index, item) {
+		
+		//Guardar id del personaje
+		characterId = item.id;
+		
+		//Agregar id como dato al personaje
+		$characterTemplate.find('.character').data("characterId", characterId);
+
 		//Guardar el nombre
 		characterName = item.name;
 		
@@ -95,7 +117,7 @@ $(document).ready(function() {
 		});
 
 		//Mostrar la plantilla en la páigna
-		$characterTemplate.clone().appendTo($charactersContainer);
+		$characterTemplate.clone(true).appendTo($charactersContainer);
 
 		//Reiniciar Related comics para el siguiente personaje
 		$characterTemplate.find('.related-comics ul').html("");
@@ -124,8 +146,104 @@ $(document).ready(function() {
 				//Para cada resultado
 				$.each(response.data.results, function(index, item) {
 					showFoundCharacter(index, item);
-				});	
+				});
+
+				//Agregar botones de ver mas
+				addViewMoreButtons();
 			});
+		}	
+	}
+
+	//Alternar comics cargados
+	function toggleLoadedComics($character) {
+		//Para cada comic de la lista de comics
+		$character.find('.related-comics li').each( function () {
+			//Si está visible
+			if ($(this).is(":visible")) {
+				//Ocultarlo
+				$(this).hide();
+			} else {
+				//Mostrarlo
+				$(this).show();
+			}
+		});		
+	}
+
+	//Expandir personaje
+	function expandCharacter($character) {
+		//Minimizar todos los personajes expandidos
+		//Para cada personaje
+		$('.character').each( function () {
+			//Si está expandido
+			if ($(this).hasClass('expanded')) {
+				//Minimizarlo
+				minCharacter($(this));
+			}
+		});
+
+		//Alternar comics cargados
+		toggleLoadedComics($character);
+
+		//Guardar el ul de los comics relacionados
+		$relatedComics = $character.find('.related-comics ul');
+
+		//Guardar el id del personaje
+		characterId = $character.first().data('characterId');
+		
+		//Crear URI de los comics del personaje
+		characterComicsURI = charactersURI + "/" + characterId + "/comics";
+
+		//Crear una lista con los títulos de los últimos comics asociados al personaje
+		//Si no se han cargado todos los comics para este personaje
+		if (!$character.hasClass('loaded')) {
+			//Cargarlos
+			$.getJSON( characterComicsURI, {
+				apikey: "b7d3e92174566cf327622bfd1a930a14",
+				orderBy: "-modified"
+			}, function( response ) {
+				//Para cada resultado
+				$.each(response.data.results, function(index, item) {
+					//Agregar el titulo del comic a la lista de related comics
+					//Guardar titulo del comic
+					comicTitle = item.title;
+
+					//Agregar el título a la ul de related comics
+					$relatedComics.append('<li class="col">' + comicTitle + '</li>');
+				});
+
+				//Marcar el personaje como cargado para no volver a cargar los datos de nuevo
+				$character.addClass('loaded');
+			});
+		}
+
+		//Cambiar texto del botón por "view less"
+		$character.find('button').html('View less');
+
+		//Agregar clase expanded al personaje
+		$character.addClass('expanded');
+	}
+
+	//Minimizar personaje
+	function minCharacter($character) {
+		//Alternar comics cargados
+		toggleLoadedComics($character);
+
+		//Remover clase expanded al personaje
+		$character.removeClass('expanded');
+
+		//Cambiar texto del botón por "view more"
+		$character.find('button').html('View more');
+	}
+
+	//Alternar personaje
+	function toggleCharacter($character) {
+		//Si el personaje está expandido
+		if ($character.hasClass('expanded')) {
+			//Minimizarlo
+			minCharacter($character);
+		} else {
+			//Expandirlo
+			expandCharacter($character);
 		}	
 	}
 
@@ -137,7 +255,7 @@ $(document).ready(function() {
 	//CUANDO SE HAGA UNA BUSQUEDA MOSTRAR LOS PRIMEROS 10 RESULTADOS DE PERSONAJES
 	//Cuando se presione el botón buscar
 	$searchButton.click(function(event) {
-		//Buscar los personajes que cuyo nombre comienza con el termino de búsqueda
+		//Buscar los personajes que cuyo nombre comienza con el término de búsqueda
 		searchCharacters();
 	});
 
@@ -159,4 +277,14 @@ $(document).ready(function() {
 			searchCharacters();
 		}
 	});
+
+	//AL HACER CLICK EN EL BOTÓN DE VER MAS
+	$viewMoreButton.click( function() {
+		//Guardar el personaje asociado al botón
+		$character = $(this).parents('.character');
+
+		//Expandir el personaje asociado al botón
+		toggleCharacter($character);
+	});
+		
 });
